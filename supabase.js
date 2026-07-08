9e4bb16deb210154db116c23a2fa41c288c49bca
Vasileva22 <olesyav03@mail.ru>
2026-07-08 22:22:06 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Добавляем безопасные заглушки, чтобы сборщик Vercel не падал, если переменные окружения проверяются до запуска
+// Добавляем безопасные текстовые заглушки на время сборки на Vercel. 
+// При запуске живого сайта Next.js автоматически заменит их на ваши настоящие переменные окружения.
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'placeholder-key';
 
