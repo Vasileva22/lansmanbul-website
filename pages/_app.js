fe73ef04db9f223b321062fc938a13bf4d073bbc
Vasileva22 <olesyav03@mail.ru>
2026-07-16 16:06:31 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,17 @@
+import { Mulish } from 'next/font/google';
+import '../styles/globals.css';
 
+// Настраиваем оптимизированную загрузку шрифта Mulish
+const mulish = Mulish({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-mulish', // Создаем CSS-переменную для использования в стилях
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={mulish.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
