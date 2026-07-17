dd2b40810a98a0580633615fcca0f4cf4bf55cce
Vasileva22 <olesyav03@mail.ru>
2026-07-17 17:05:24 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,7 @@ export default function Home({ initialProperties }) {
         }
       }, 800);
     }
-  }, [router.isReady, router.query]);
+  }, [router.isReady, router.asPath]);
 
   useEffect(() => {
     const consent = localStorage.getItem('cookie_consent');
